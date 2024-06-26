75f2aefe1b2b830b488de8a5e4410ba21c388368
Damao <3034906016@qq.com>
2024-06-26 12:21:45 +0800

    import超级无敌疯狂哈基米

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
-axios.defaults.timeout = 10000000; // 超时时间
-axios.defaults.baseURL = "http://127.0.0.1:8080/"; // http://127.0.0.1:4523/m1/3898618-0-default"; //"http://127.0.0.1:8080";
+axios.defaults.timeout = 10000000;
+axios.defaults.baseURL = "http://192.168.1.7:8080/";
 // 数据格式转换
 axios.defaults.transformRequest = function (data) {
   data = JSON.stringify(data);
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   publicPath: "./",
   transpileDependencies: true,
   devServer: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 8080,
     open: true,
     proxy: {
